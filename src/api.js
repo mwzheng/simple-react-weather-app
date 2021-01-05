a9793f882f2db59224e974ae4ed2953774182525
Mickey <mickey.zheng@yahoo.com>
2021-01-05 17:46:26 -0500

    added api obj for api key and modified some initial files

---
+++ src/api.js
@@ -0,0 +1,7 @@
+const API_OBJ = {
+    key: 'Store your API Key here',
+    base: 'https://api.weatherbit.io/v2.0/current?',
+    icon: 'https://www.weatherbit.io/static/img/icons/'
+}
+
+export default API_OBJ;
